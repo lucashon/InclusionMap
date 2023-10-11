3b17520f4de14c7174980728c18a234a0325f238
Lucas Honorio <110117686+lucashon@users.noreply.github.com>
2023-10-11 17:01:05 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+// Modal Cadastro
+const modal = document.getElementById('modal')
+const btn = document.getElementById('btn')
+const close = document.getElementById('close')
+btn.addEventListener('click',()=>{
+    modal.style.display = 'flex '
+})
+close.addEventListener('click',()=>{
+    modal.style.display = 'none'
+})
+
+// Modal Entrar
+const modalLogin = document.getElementById('modalLogin')
+const btnLogin = document.getElementById('btnLogin')
+const closeLogin = document.getElementById('closeLogin')
+btnLogin.addEventListener('click',()=>{
+    modalLogin.style.display = 'flex '
+})
+closeLogin.addEventListener('click',()=>{
+    modalLogin.style.display = 'none'
+})
+
+window.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') {
+      modal.style.display = 'none'
+    }
+})
+
+window.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') {
+      modalLogin.style.display = 'none'
+    }
+})
+
+const submit = document.getElementById('submit-cadastro')
+submit.addEventListener('click',()=>{
+    alert('Ok')
+})
+
